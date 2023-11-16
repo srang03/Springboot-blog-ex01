909fc843b6c57550a753d08a07e7059ba9c92590
srang03 <srang_@naver.com>
2023-11-16 20:50:34 +0900

    환경세팅 완료

---
+++ src/main/java/com/example/demo/test/BlogControllerTest.java
@@ -0,0 +1,14 @@
+package com.example.demo.test;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BlogControllerTest {
+	@GetMapping("/test/hello")
+	public String hello() {
+		return "<h1>hello string boot</h1>";
+	}
+
+}
